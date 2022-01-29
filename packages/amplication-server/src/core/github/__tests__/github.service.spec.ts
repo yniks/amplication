b4fae0f9b9917f2c5f10fb3e737ac2282305e314
tupe12334 <ofek.gabay.he@gmail.com>
2022-01-29 06:00:38 -0500

    refactor(server): remove code from test

--- packages/amplication-server/src/core/github/__tests__/github.service.spec.ts
+++ packages/amplication-server/src/core/github/__tests__/github.service.spec.ts
@@ -15,34 +15,35 @@ describe('GithubService', () => {
       googleSecretManagerService,
       tokenExtractor
     );
-    const adminUiFiles: Module[] = [
-      { code: '', path: 'admin-ui/src/entity/OrderShow.tsx' }
-    ];
-    const customerCustomFiles: Module[] = [
-      { code: '', path: 'server/src/entity/entity.service.ts' },
-      { code: '', path: 'server/src/auth/auth.service.ts' },
-      { code: '', path: 'server/src/auth/token.service.ts' },
-      { code: '', path: 'server/src/auth/password.service.ts' },
-      { code: '', path: 'server/src/auth/UserInfo.ts' }
+    const placeHolderCode = '';
+    const adminUiFiles: string[] = ['admin-ui/src/entity/OrderShow.tsx'];
+    const customerCustomFiles: string[] = [
+      'server/src/entity/entity.service.ts',
+      'server/src/auth/auth.service.ts',
+      'server/src/auth/token.service.ts',
+      'server/src/auth/password.service.ts',
+      'server/src/auth/UserInfo.ts'
       //#region auth strategy
       // { code: '', path: 'server/src/auth/jwt/jwt.strategy.ts' }, //TODO
       // { code: '', path: 'server/src/auth/basic/basic.strategy.ts' }, //TODO
       //#endregion
     ];
-    const amplicationFiles: Module[] = [
-      { code: '', path: 'server/src/auth/base/auth.service.base.ts' },
+    const amplicationFiles: string[] = [
+      'server/src/auth/base/auth.service.base.ts',
       ...adminUiFiles // make sure that any admin-ui file is amplication file
     ];
 
-    test.each(amplicationFiles)('%s', module => {
-      const expectedResult = JSON.stringify({ [module.path]: module.code });
+    test.each(amplicationFiles)('%s', path => {
+      const module: Module = { code: placeHolderCode, path };
+      const expectedResult = JSON.stringify({ [path]: placeHolderCode });
       const result = JSON.stringify(githubService.prepareFilesForPr([module]));
 
       expect(result).toBe(expectedResult);
     });
-    test.each(customerCustomFiles)('%s', module => {
+    test.each(customerCustomFiles)('%s', path => {
+      const module: Module = { code: placeHolderCode, path };
       const result = githubService.prepareFilesForPr([module]);
-      const isOverwritesFunction = typeof result[module.path] === 'function';
+      const isOverwritesFunction = typeof result[path] === 'function';
       expect(isOverwritesFunction).toBe(true);
     });
   });
